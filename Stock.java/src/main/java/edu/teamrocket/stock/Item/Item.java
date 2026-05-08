b35120f3a82f043e6e573ef499d5e3437bc3c62a
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-05-08 14:18:31 +0200

    feat: Definir la clase publica sneaker
    Definir la clase publica sneaker implementando la interfaz Item

--- Stock.java/src/main/java/edu/teamrocket/stock/Item/Item.java
+++ Stock.java/src/main/java/edu/teamrocket/stock/Item/Item.java
@@ -1,10 +1,15 @@
 package edu.teamrocket.stock.Item;
+import java.util.List;
 
 public interface Item {
 
     int getBid();
     int getAsk();
     int getSale();
+    void setBid(int bid);
+    void setAsk(int ask);
+    void setSale(int sale);
+    list<Offer> offers();
 }
 
 
---
+++ Stock.java/src/main/java/edu/teamrocket/stock/Item/Sneaker.java
@@ -0,0 +1,53 @@
+package edu.teamrocket.stock.Item;
+
+public class Sneaker  implements Item{
+    private String style;
+    private String name;
+    private int sale;
+    private int ask;
+    private int bid;
+
+    public Sneaker(String style, String name){
+        this.style = style;
+        this.name = name;
+    }
+
+    public Object getStyle(){
+        return this.style;
+    }
+
+    public Object getName(){
+        return this.name;
+    }
+
+    @Override
+    public int getSale(){
+        return this.sale;
+    }
+
+    @Override
+    public void setSale(int sale){
+        this.sale = sale;
+    }
+
+    @Override
+    public int getAsk(){
+        return this.ask;
+    }
+
+    @Override
+    public void setAsk(int ask){
+        this.ask = ask;
+    }
+
+    @Override
+    public int getBid(){
+        return this.bid;
+    }
+
+    @Override
+    public void setBid(int bid){
+        this.bid = bid;
+    }
+
+}
