7c4632c0c8864120c01df4431b3c5b82ddd769d6
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-05-15 10:51:00 +0200

    fix: Arreglar codigo
    Arreglar el codigo cuyo objetivo tenia buscar la puja más baja cuando en el diagrama ponia la oferta de venta mas baja

--- src/main/java/edu/teamrocket/stock/criteria/MinAsk.java
+++ src/main/java/edu/teamrocket/stock/criteria/MinAsk.java
@@ -2,15 +2,15 @@ package edu.teamrocket.stock.criteria;
 
 import edu.teamrocket.stock.item.Item;
 import edu.teamrocket.stock.item.Offer;
-import edu.teamrocket.stock.item.Bid;
+import edu.teamrocket.stock.item.Ask;
 import java.util.List;
 import java.util.Collections;
 
-public class MinBid implements Criteria {
+public class MinAsk implements Criteria {
     @Override
     public List<Offer> checkCriteria(Item item) {
         return item.offers().stream()
-                .filter(offer -> offer instanceof Bid)
+                .filter(offer -> offer instanceof Ask)
                 .min(Offer::compareTo)
                 .map(Collections::singletonList)
                 .orElse(Collections.emptyList());
